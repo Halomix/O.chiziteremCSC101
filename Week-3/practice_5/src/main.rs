a44c830f5c1d16a3fe6004abc285478f0212c9de
halomix <chizzyokechukwu2@gmail.com>
2022-11-19 19:21:42 +0100

    My third commit

---
+++ Week-3/practice_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let special_character = '@';  //default
+    let alphabet:char = 'A';
+    let surname = "okechukwu";
+    let first_name = "chiziterem";
+    let middle_name = "fortune";
+
+    println!("");
+    println!("Special character{}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+    
+}
