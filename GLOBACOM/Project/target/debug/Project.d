abe14ba5fe296c866c1ccb44a8437143b667cb98
Halomix <Chizzyokechukwu2@gmail.com>
2023-01-28 14:08:06 -0800

    my commit

--- GLOBACOM/Project/target/debug/Project.d
+++
@@ -1,1 +0,0 @@
-C:\Users\User\Desktop\CSC101\O.chiziteremCSC101\GLOBACOM\Project\target\debug\Project.exe: C:\Users\User\Desktop\CSC101\O.chiziteremCSC101\GLOBACOM\Project\src\main.rs
--- GLOBACOM/Project/target/debug/deps/Project.d
+++
@@ -1,5 +0,0 @@
-C:\Users\User\Desktop\CSC101\O.chiziteremCSC101\GLOBACOM\Project\target\debug\deps\Project.exe: src\main.rs
-
-C:\Users\User\Desktop\CSC101\O.chiziteremCSC101\GLOBACOM\Project\target\debug\deps\Project.d: src\main.rs
-
-src\main.rs:
